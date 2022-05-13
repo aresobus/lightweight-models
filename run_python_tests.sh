788a41ce251394ef29265c9a1427854329225d4b
Alex Lebedyev <aresobus@gmail.com>
2022-05-13 18:00:00 +0200

    update test utils

--- run_python_tests.sh
+++ run_python_tests.sh
@@ -14,6 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
+
 # Run all Python tests in this tfjs-models repository.
 
 set -e
