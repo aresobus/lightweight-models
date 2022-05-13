906d7066ab4155460b262921080a3fd7f38d2a08
Alex Lebedyev <aresobus@gmail.com>
2022-05-13 18:00:00 +0200

    minor updates for configs

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,6 +3,7 @@
 We'd love to accept your patches and contributions to this project. There are
 just a few small guidelines you need to follow.
 
+
 ## Contributor License Agreement
 
 Contributions to this project must be accompanied by a Contributor License
--- test_util.ts
+++ test_util.ts
@@ -3,6 +3,7 @@
  * Copyright 2021 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
+ *
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -22,17 +23,19 @@
 // tslint:disable-next-line
 export function runTests(jasmine_util: any): void {
   // tslint:disable-next-line:no-require-imports
-  const jasmineCtor = require('jasmine');
+  const jasmineCtor = require("jasmine");
 
   Error.stackTraceLimit = Infinity;
 
-  process.on('unhandledRejection', e => {
+  process.on("unhandledRejection", (e) => {
     throw e;
   });
 
-  jasmine_util.setTestEnvs([{name: 'test-cpu', backendName: 'cpu', flags: {}}]);
+  jasmine_util.setTestEnvs([
+    { name: "test-cpu", backendName: "cpu", flags: {} },
+  ]);
 
   const runner = new jasmineCtor();
-  runner.loadConfig({spec_files: ['src/**/*_test.ts'], random: false});
+  runner.loadConfig({ spec_files: ["src/**/*_test.ts"], random: false });
   runner.execute();
 }
